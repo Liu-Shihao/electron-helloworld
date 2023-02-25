90e6b265ed968a6557664453b8ccf99467a593b2
LiuShihao <liush99@foxmail.com>
2023-02-25 23:58:54 +0800

    add dependent library

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
-## Electron快速入门
+# :rocket: Electron 快速入门
 
-```sh
+Electron 是一个基于 Chromium 和 Node.js 的框架，用于编写跨平台的桌面应用程序。它使用 HTML、CSS 和 JavaScript 这些 web 技术来开发应用程序，这些技术的优势在于可以跨平台，也可以很容易的进行编写和维护。
+
+## :zap: 为什么选择 Electron?
+
+- 完全免费：Electron 是完全免费的开源框架，并且不需要任何许可证，可以自由使用。
+
+- 跨平台：Electron 可以在 Windows、macOS 和 Linux 上运行，因此可以轻松地部署到不同的操作系统上。
+
+- 快速开发：使用 Electron 开发应用程序可以节省开发时间，因为它使用了 web 技术，开发者可以利用熟悉的 HTML、CSS 和 JavaScript 来开发应用程序。
+
+## :sparkles: Electron 快速入门
+
+本教程将指导你如何使用 Electron 快速构建一个桌面应用程序。
+
+- 第一步：安装 Node.js 和 Electron
+- 第二步：准备应用程序的基本文件
+- 第三步：安装 Electron 依赖
+- 第四步：编写基本的 HTML、CSS 和 JavaScript 代码
+- 第五步：测试应用程序
+- 第六步：打包应用程序以供部署
+
+
+## 启动
+```
+npm install electron
+npm install http-proxy-middleware
+npm install express
+
 npm start
 ```
+## :fire: 总结
+
+Electron 是一个易于使用的框架，可以帮助你快速构建跨平台的桌面应用程序。它使用 web 技术，可以轻松地利用 HTML、CSS 和 JavaScript 来开发应用程序，并且拥有跨平台的优势，可以让你节省开发时间，实现更快的部署。
+
+
--- main.js
+++ main.js
@@ -1,29 +1,41 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require('express');
 
-function createWindow () {
-  const win = new BrowserWindow({
+const server = express();
+const port = 3000;
+let mainWindow;
+
+//创建桌面窗口
+function createWindow() {
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
 
-  win.loadFile('index.html')
+  // 设置代理，将请求转发到http://127.0.0.1:5000
+server.use('/', createProxyMiddleware({
+  target: 'http://127.0.0.1:5000',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/': '/hello'
+  }
+}));
+
+// 监听端口号
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 }
 
-app.whenReady().then(() => {
-  createWindow()
+app.on('ready', createWindow);
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
